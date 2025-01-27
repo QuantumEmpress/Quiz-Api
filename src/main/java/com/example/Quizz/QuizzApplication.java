7d3a513e42b0e066fc54de3332a7ae412addc3d7
Onanta Bassey <onantabasseyvee@gmail.com>
2025-01-27 19:32:50 +0100

    Fixed, works

--- src/main/java/com/example/Quizz/QuizzApplication.java
+++ src/main/java/com/example/Quizz/QuizzApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = "com.example.Quizz")
+@SpringBootApplication
 public class QuizzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzApplication.class, args);
